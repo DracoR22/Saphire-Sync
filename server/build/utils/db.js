56c8adbf03911091b6e3231a5d97a5a2cc33f424
DracoR22 <rdraco039@gmail.com>
2023-09-27 17:29:36 -0500

    server4

--- server/build/utils/db.js
+++ server/build/utils/db.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 require('dotenv').config();
-const dbUrl = process.env.DB_url || '';
+const dbUrl = process.env.DB_URL || '';
 const connectDB = async () => {
     try {
         await mongoose_1.default.connect(dbUrl)
