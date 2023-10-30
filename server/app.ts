a3a836d36fb0e47eef818ea03ff5429b12259e89
DracoR22 <rdraco039@gmail.com>
2023-10-30 12:51:19 -0500

    30102023

--- server/app.ts
+++ server/app.ts
@@ -19,14 +19,9 @@ export const app = express()
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
-    origin: ['https://saphire-sync.vercel.app'],
+    origin: ['https://saphire-sync.vercel.app', 'http://localhost:3000'],
     credentials: true
 }))
-
-// Testing Api
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({success: true,  message: "API is working"})
-})
 
 // Limit Api Calls
 const limiter = rateLimit({
@@ -50,6 +45,11 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     const err = new Error(`Route ${req.originalUrl} not found`) as any
     err.statusCode = 404
     next(err)
+})
+
+// Testing Api
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({success: true,  message: "API is working"})
 })
 
 // Middleware Calls
